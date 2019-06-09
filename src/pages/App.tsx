43e681c7812049a9b1e2ff3e584555556875578a
steve xu <stevexugc@gmail.com>
2019-06-09 11:06:33 +0800

    feat: Add Sass Module

---
+++ src/pages/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from "./App.module.scss";
+
+const App: React.FC = () => {
+    return (
+        <div className={styles.container}>
+            kindle
+        </div>
+    );
+}
+
+export default App;
